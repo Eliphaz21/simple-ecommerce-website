e43203caaa1662666a6f0b9dc398c4ebfa9eff71
yeabsra <yeabsraandinet19@gmail.com>
2025-09-28 17:17:04 +0300

    move checkout page into

--- reat-high-video/src/pages/Homepage.jsx
+++ reat-high-video/src/pages/Homepage.jsx
@@ -3,6 +3,7 @@
  export function Homepage() {
     return (
         <>
+        <title>Eccommerce project</title>
          <div className="header">
       <div className="left-section">
         <a href="index.html" className="header-link">
